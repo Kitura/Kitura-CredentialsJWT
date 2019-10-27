bb270489583f81b990b7e3c5ce915823eca54ee4
Christopher Prince <chris@SpasticMuffin.biz>
2019-10-27 15:38:42 -0600

    Update Package.swift for Swift 4.

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -14,7 +14,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", from: "2.4.0"),
+        .package(url: "https://github.com/crspybits/Kitura-Credentials.git", .branch("master")),
+        // .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", from: "2.4.0"),
         .package(url: "https://github.com/IBM-Swift/Swift-JWT.git", from: "3.1.0"),
     ],
     targets: [
